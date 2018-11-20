6f748b13e392ba074822270afc2a8a155b4c80ee
mmohamud <mohamed.a.mohamud@helsinki.fi>
2018-11-20 15:55:49 +0200

    BookTip & WebTip luotu

--- MyReadingTips/src/main/java/mytips/model/BookTip.java
+++ MyReadingTips/src/main/java/mytips/model/BookTip.java
@@ -5,6 +5,8 @@
  */
 package mytips.model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author mmohamud
@@ -13,5 +15,69 @@ public class BookTip {
     private String Author;
     private String Name;
     private String ISBN;
+    private String summary;
+    private ArrayList<String> tags;
+    private ArrayList<String> preliminaryKnowldge;
+    private ArrayList<String> relatedCourses;
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public void setAuthor(String Author) {
+        this.Author = Author;
+    }
+
     
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+    
+    
+
+    public ArrayList<String> getTags() {
+        return tags;
+    }
+    
+    public void addTag(String tag) {
+        this.tags.add(tag);
+    }
+    
+    public ArrayList<String> getPreliminaryKnowldge() {
+        return preliminaryKnowldge;
+    }
+    
+   public void addPreliminaryKnowledge(String knowledge) {
+       this.preliminaryKnowldge.add(knowledge);
+   }
+
+    public ArrayList<String> getRelatedCourses() {
+        return relatedCourses;
+    }
+   
+   public void addRelatedCourse(String courseName) {
+       this.relatedCourses.add(courseName);
+   }
+   
 }
