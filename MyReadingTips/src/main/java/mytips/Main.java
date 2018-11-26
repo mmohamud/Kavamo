7f00317e5af09ec40459cb8902dc2326a551eda1
mmohamud <mohamed.a.mohamud@helsinki.fi>
2018-11-26 15:13:45 +0200

    tietokanta luotu

--- MyReadingTips/src/main/java/mytips/Main.java
+++ MyReadingTips/src/main/java/mytips/Main.java
@@ -11,7 +11,7 @@ import javafx.stage.Stage;
 import mytips.model.ReadingTip;
 import mytips.model.ReadingTipManager;
 import mytips.model.TipManager;
-
+import java.sql.*;
 
 
 public class Main extends Application {
@@ -37,16 +37,31 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
+    
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         //launch(args);
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:testi.db");
+
+        PreparedStatement statement = connection.prepareStatement("SELECT 1");
+
+        ResultSet resultSet = statement.executeQuery();
+
+        if (resultSet.next()) {
+            System.out.println("Hei tietokantamaailma!");
+        } else {
+            System.out.println("Yhteyden muodostaminen epäonnistui.");
+        }
+    
         ReadingTipManager readingTipManager = new ReadingTipManager();
         TextualUI ui = new TextualUI(readingTipManager);
         ui.start();
         System.out.println("Lopetetaan");
+        
     }
+
+    
     
 }
