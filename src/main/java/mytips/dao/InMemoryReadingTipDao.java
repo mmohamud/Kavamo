1c5239c4d9907d168426c878788b6da8dc3d21a7
mmohamud <mohamed.a.mohamud@helsinki.fi>
2018-12-05 22:29:02 +0200

    CheckStyle huomiot korjattu

--- src/main/java/mytips/dao/InMemoryReadingTipDao.java
+++ src/main/java/mytips/dao/InMemoryReadingTipDao.java
@@ -18,13 +18,12 @@ public class InMemoryReadingTipDao implements Dao {
 
     private ArrayList<ReadingTip> readingTips = new ArrayList<>();
 
-    public InMemoryReadingTipDao() {
+    /*public InMemoryReadingTipDao() {
         readingTips.add(new ReadingTip("Robert Martin", "Clean Code: A Handbook of Agile Software Craftsmanship", "", "","book"));
         readingTips.add(new ReadingTip("Margaret Atwood", "Orjattaresi", "", "Margaret Atwoodin Orjattaresi on vavahduttava dystopia lähitulevaisuuden Yhdysvalloista, jossa vanhatestamentilliset fundamentalistit ovat ottaneet vallan. Yli 30 vuotta ensijulkaisunsa jälkeen romaanin teemat vapaudesta ja naisten oikeuksista ovat nyt ajankohtaisempia kuin koskaan. Margaret Atwood on kanadalainen kirjailija, joka on pitkän uransa aikana kirjoittanut yli 40 teosta ja saanut useita merkittäviä kirjallisuuspalkintoja.", "book"));
         readingTips.add(new ReadingTip("Nicola Apicella", "Consistency models", "", "", "web"));
         readingTips.add(new ReadingTip("", "Merge sort algorithm", "", "Hyvä selitys merge sortin toiminnasta esimerkin avulla", "web"));
-    }
-
+    }*/
     @Override
     public Object findOne(Object key) throws SQLException {
         //To change body of generated methods, choose Tools | Templates.
@@ -51,7 +50,8 @@ public class InMemoryReadingTipDao implements Dao {
 
     @Override
     public Object findOneById(int key) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet.");
+//To change body of generated methods, choose Tools | Templates.
     }
 
 }
