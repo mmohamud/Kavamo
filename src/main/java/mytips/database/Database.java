c006408be7b818420d90ea1f9cc1dcf87eafd095
MWargelin <matias.wargelin@gmail.com>
2018-12-07 18:31:35 +0200

    Tietokannan luominen refaktoroitu pois Mainista Database-luokkaan

--- src/main/java/mytips/database/Database.java
+++ src/main/java/mytips/database/Database.java
@@ -3,14 +3,13 @@ package mytips.database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Database {
-
     private String databaseAddress;
 
     public Database(String databaseAddress) throws ClassNotFoundException {
         this.databaseAddress = databaseAddress;
-//        System.out.println("Database syntyy nyt täällä");
     }
 
     public Connection getConnection() throws SQLException {
@@ -25,4 +24,25 @@ public class Database {
 
         return DriverManager.getConnection("jdbc:sqlite:readingtips.db");
     }
+	
+	public void init() throws SQLException {
+		Connection conn = this.getConnection();
+        Statement statement = conn.createStatement();
+        statement.execute(createTables());
+        statement.close();
+        conn.close();
+	}
+	
+	private String createTables() {
+		return "CREATE TABLE IF NOT EXISTS ReadingTip ("
+            + "id integer PRIMARY KEY,"
+            + "author varchar(40),"
+            + "title varchar(40),"
+            + "summary varchar(200),"
+            + "comment varchar(100),"
+            + "isbn varchar(20),"
+            + "url varchar(100),"
+            + "type varchar(20)"
+            + ");";
+	}
 }
